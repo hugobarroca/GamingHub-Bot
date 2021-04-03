ba360ad65c84d3681bb064052e6525086041ed15
Hugo Barroca <hugobarroca98@gmail.com>
2021-04-03 19:25:00 +0100

    Add project files.

---
+++ GamingHubBot/Modules/Commands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TutorialBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("pong motherfucker");
+        }
+
+        [Command("help")]
+        public async Task Help()
+        {
+            string message = "";
+            message += "**!help**: Displays this informative message.\n";
+            message += "**!ping**: Respondes with a pong to confirm online status.\n";
+            await ReplyAsync(message);
+        }
+    }
+}
