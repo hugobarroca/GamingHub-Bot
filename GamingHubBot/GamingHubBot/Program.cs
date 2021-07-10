7c1bb0e18fd293a583cecaaa7b70c6ec070e2a8e
Hugo Barroca <hugobarroca98@gmail.com>
2021-07-10 15:36:14 +0100

    Added connectivity to discord servers.

--- GamingHubBot/GamingHubBot/Program.cs
+++ GamingHubBot/GamingHubBot/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace GamingHubBot
 {
     class Program
     {
+        private DiscordSocketClient _client;
+
 		public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
 		{
-			Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
 		}
 	}
 }
