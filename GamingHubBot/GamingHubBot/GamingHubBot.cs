a8677b472db37aa7fe149d373e32776ed2632e4c
Hugo Barroca <hugobarroca98@gmail.com>
2021-12-08 22:33:33 +0000

    Removed Topshelf.

--- GamingHubBot/GamingHubBot/GamingHubBot.cs
+++ GamingHubBot/GamingHubBot/GamingHubBot.cs
@@ -3,9 +3,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace GamingHubBot
--- GamingHubBot/GamingHubBot/Program.cs
+++ GamingHubBot/GamingHubBot/Program.cs
@@ -1,45 +1,18 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using Discord.WebSocket;
+using System;
 using System.Threading.Tasks;
-using Topshelf;
-using Topshelf.Runtime.DotNetCore;
 
 namespace GamingHubBot
 {
     class Program
     {
-
         public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
         {
-            var exitCode = HostFactory.Run(x =>
-            {
-                if (
-                  RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ||
-                  RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-                    )
-                {
-                    x.UseEnvironmentBuilder(
-                      target => new DotNetCoreEnvironmentBuilder(target)
-                    );
-                }
-                x.Service<GamingHubBot>(s =>
-                {
-                    s.ConstructUsing(gaminghubbot => new GamingHubBot());
-                    s.WhenStarted(gaminghubbot => gaminghubbot.Start());
-                    s.WhenStopped(gaminghubbot => gaminghubbot.Stop());
-                });
-
-                x.RunAsNetworkService();
-
-                x.SetServiceName("GamingHubBot");
-                x.SetDisplayName("Gaming Hub Bot");
-                x.SetDescription("This is the gaming hub bot service, which services all discord requests.");
-            });
-
-            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
-            Environment.ExitCode = exitCodeValue;
+            var bot = new GamingHubBot();
+            bot.Start();
+            await Task.Delay(-1);
         }
 
     }
