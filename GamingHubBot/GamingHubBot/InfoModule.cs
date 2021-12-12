8a799e1c7ae1c6a9af28fa7fe543623b6dfb6964
Hugo Barroca <hugobarroca98@gmail.com>
2021-12-12 16:56:11 +0000

    .NET worflow test commit.

--- GamingHubBot/GamingHubBot/InfoModule.cs
+++ GamingHubBot/GamingHubBot/InfoModule.cs
@@ -31,7 +31,7 @@ namespace GamingHubBot
             message += "**!addrole** *{role}*: Adds specified role to the user. Use quotations marks for roles with spaces.\n";
             message += "**!removerole** *{role}*: Removes specified role from the user. Use quotations marks for roles with spaces.\n";
             message += "**!weeb**: Gives you a random anime quote.\n";
-            message += "test.";
+            message += "test.2";
             await ReplyAsync(message);
         }
 
