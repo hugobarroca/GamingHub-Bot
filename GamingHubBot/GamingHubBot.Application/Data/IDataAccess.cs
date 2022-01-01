8b27274c453ae36d3325e92045164ace3917ad1f
Hugo Barroca <hugobarroca98@gmail.com>
2022-01-01 21:24:42 +0000

    Added missing semicolon.

--- GamingHubBot/GamingHubBot.Application/Data/IDataAccess.cs
+++ GamingHubBot/GamingHubBot.Application/Data/IDataAccess.cs
@@ -8,6 +8,6 @@ namespace GamingHubBot.Data
         public Task SynchronizeRolesAsync(IEnumerable<Role> rolesToInsert, IEnumerable<Role> rolesToRemove);
         public Task<IEnumerable<Role>> GetRolesAsync();
         public Task<List<Color>> GetColorsAsync();
-        public Task AddRoleToPermittedList(ulong id)
+        public Task AddRoleToPermittedList(ulong id);
     }
 }
